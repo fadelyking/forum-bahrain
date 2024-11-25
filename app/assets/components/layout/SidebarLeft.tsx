021d9e5f28abbbe223efabb7d34e17157ad4153f
fadelyking <fadely195@gmail.com>
2024-11-25 11:44:18 +0300

    create sidebar left with all categories

---
+++ app/assets/components/layout/SidebarLeft.tsx
@@ -0,0 +1,28 @@
+"use client";
+import useCategoriesStore from "@/app/stores/categoriesStore";
+import { Box } from "@radix-ui/themes";
+import { v4 as uuidv4 } from "uuid";
+import ForumButton from "../common/Button";
+import SearchBar from "../common/TextField";
+export default function SidebarLeft() {
+	const categories = useCategoriesStore((state) => state.categoriesList);
+
+	return (
+		<Box className="col-start-6 col-end-7 max-w-full">
+			<div className=" flex items-end justify-end">
+				<ul className="noto_kufi flex flex-col gap-3 font-bold justify-end items-end text-md p-6 bg-[#272727] text-[#bdbdbd] rounded-lg">
+					<div className="flex flex-col gap-3 items-center justify-center max-w-52">
+						<ForumButton>ابتد موضوع جديد</ForumButton>
+						<SearchBar />
+					</div>
+					{categories.map((category) => (
+						<li key={uuidv4()}>
+							<span>{category.icon}</span>
+							<span>{category.name}</span>
+						</li>
+					))}
+				</ul>
+			</div>
+		</Box>
+	);
+}

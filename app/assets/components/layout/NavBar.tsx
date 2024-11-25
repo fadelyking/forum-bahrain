6fe3c7675a0b274b45d7ded846316f66b8ae2da5
fadelyking <fadely195@gmail.com>
2024-11-26 01:05:03 +0300

    create navbar

---
+++ app/assets/components/layout/NavBar.tsx
@@ -0,0 +1,33 @@
+import { SunIcon } from "@radix-ui/react-icons";
+import ForumButton from "../common/Button";
+
+export default function NavBar() {
+	return (
+		<div className="bg-[#272727] p-5 px-20 flex col-start-1 col-span-full justify-end items-center">
+			<ul className="noto_kufi flex gap-5 text-white items-center">
+				<li className="hover:text-[#2bad73] cursor-pointer">
+					الأكثر تداولاً
+				</li>
+				{/* Trending */}
+				<li className="hover:text-[#2bad73] cursor-pointer">
+					منشورات جديدة
+				</li>
+				{/* New Posts */}
+				<li className="hover:text-[#2bad73] cursor-pointer">نشاطك</li>
+				{/* Your Activity */}
+				<li className="hover:text-[#2bad73] cursor-pointer">
+					تسجيل الدخول
+				</li>
+				{/* Login/Profile */}
+				<li className="hover:text-[#2bad73] cursor-pointer">
+					<ForumButton>أنشئ منشوراً جديد</ForumButton>
+				</li>
+				{/* Create New Thread */}
+				<li className="hover:text-[#2bad73] cursor-pointer">
+					<SunIcon />
+				</li>
+				{/* Theme Toggle */}
+			</ul>
+		</div>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import SidebarLeft from "./assets/components/layout/SidebarLeft";
 import MainContent from "./assets/components/layout/MainContent";
+import NavBar from "./assets/components/layout/NavBar";
 export default function Homepage() {
 	return (
-		<div className="grid grid-cols-7 gap-12">
-			<MainContent />
-			<SidebarLeft />
-			{/*<SidebarRight />*/}
-		</div>
+		<>
+			<div className="grid grid-cols-7 gap-12">
+				<NavBar />
+				<MainContent />
+				<SidebarLeft />
+				{/*<SidebarRight />*/}
+			</div>
+		</>
 	);
 }

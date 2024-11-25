bcdd7764dd6fa2eb83cf724618544a19b26f5fc9
fadelyking <fadely195@gmail.com>
2024-11-26 01:06:21 +0300

    pass down comments prop

--- app/assets/components/layout/MainContent.tsx
+++ app/assets/components/layout/MainContent.tsx
@@ -1,15 +1,17 @@
 "use client";
-import { Box } from "@radix-ui/themes";
+
 import useThreadsStore from "@/app/stores/threadsStore";
 import { v4 as uuidv4 } from "uuid";
 import ThreadCard from "../forumUI/threads/ThreadCard";
 import formatDate from "@/app/utils/dateFormatter";
+import useCommentsStore from "@/app/stores/commentsStore";
 
 export default function MainContent() {
 	const threads = useThreadsStore((state) => state.threadsList);
+	const comments = useCommentsStore((state) => state.commentsList);
 	if (threads) {
 		return (
-			<div className="noto_kufi col-start-3 col-end-6 row-start-1 row-span-full max-w-full">
+			<div className="noto_kufi col-start-3 col-end-6  max-w-full">
 				<ul className="noto_kufi flex flex-col gap-3 font-bold justify-end items-end text-sm p-5 bg-[#272727] text-[#bdbdbd] rounded-lg">
 					{threads.map((thread) => (
 						<li key={uuidv4()}>
@@ -19,6 +21,7 @@ export default function MainContent() {
 								content={thread.content}
 								created={formatDate(thread.created_at)}
 								likes={thread.likes}
+								comments={comments}
 							/>
 						</li>
 					))}
@@ -26,6 +29,6 @@ export default function MainContent() {
 			</div>
 		);
 	} else {
-		return "No threads currently present";
+		return "No threads currently";
 	}
 }

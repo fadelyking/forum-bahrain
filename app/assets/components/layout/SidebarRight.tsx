ae57e5f63556c9fa1ade281c41d820c076af740c
fadelyking <fadely195@gmail.com>
2024-11-26 01:06:44 +0300

    sidebar right prep

---
+++ app/assets/components/layout/SidebarRight.tsx
@@ -0,0 +1,14 @@
+export default function SideBarRight() {
+	return (
+		<div>
+			{/* Sponser */}
+			{/* Active Members or Moderators */}
+			{/* Community Events */}
+			{/* Local Businesses */}
+			{/* Forum Stats */}
+			{/* Daily Poll  */}
+			{/* Local News  */}
+			{/* Online Members */}
+		</div>
+	);
+}

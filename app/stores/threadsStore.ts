20db90d2b8aa65246783398dc556366403f9c051
fadelyking <fadely195@gmail.com>
2024-11-27 14:11:53 +0300

    get thread function hook

--- app/stores/threadsStore.ts
+++ app/stores/threadsStore.ts
@@ -1,8 +1,13 @@
 import threads from "../mocks/threads";
 import { create } from "zustand";
 import { ThreadsStore } from "../types/stores";
-const useThreadsStore = create<ThreadsStore>(() => ({
+const useThreadsStore = create<ThreadsStore>((set) => ({
+	currentThread: null,
 	threadsList: threads,
+	getThread: (id: any) => {
+		const thread = threads.find((thread) => thread.id === parseInt(id));
+		set({ currentThread: thread || null });
+	},
 }));
 
 export default useThreadsStore;

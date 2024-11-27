b5b5a0e8227a34a7cb6969c7d351b31a19554d6b
fadelyking <fadely195@gmail.com>
2024-11-27 11:08:20 +0300

    sidebar right

--- app/assets/components/layout/SidebarRight.tsx
+++ app/assets/components/layout/SidebarRight.tsx
@@ -1,14 +1,27 @@
-export default function SideBarRight() {
+import { Box } from "@radix-ui/themes";
+import Sponser from "../forumUI/widgets/Sponser";
+import RecentThreads from "../forumUI/widgets/RecentThreads";
+
+export default function SidebarRight() {
 	return (
-		<div>
-			{/* Sponser */}
-			{/* Active Members or Moderators */}
-			{/* Community Events */}
-			{/* Local Businesses */}
-			{/* Forum Stats */}
-			{/* Daily Poll  */}
-			{/* Local News  */}
-			{/* Online Members */}
+		<div className="col-start-2 row-start-2 flex flex-col gap-3">
+			<Box className="flex bg-[#272727] rounded-lg">
+				<Sponser />
+				{/* Today's Sponser */}
+				{/* Active Members or Moderators */}
+				{/* Local Businesses */}
+				{/* Forum Stats */}
+				{/* Daily Poll  */}
+				{/* Online Members */}
+			</Box>
+			<Box className="bg-[#272727] rounded-lg">
+				<RecentThreads />
+				{/* Active Members or Moderators */}
+				{/* Local Businesses */}
+				{/* Forum Stats */}
+				{/* Daily Poll  */}
+				{/* Online Members */}
+			</Box>
 		</div>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import SidebarLeft from "./assets/components/layout/SidebarLeft";
 import MainContent from "./assets/components/layout/MainContent";
 import NavBar from "./assets/components/layout/NavBar";
+import SidebarRight from "./assets/components/layout/SidebarRight";
+
 export default function Homepage() {
 	return (
 		<>
-			<div className="grid grid-cols-7 gap-12">
+			<div className="grid grid-cols-7 gap-6">
 				<NavBar />
 				<MainContent />
 				<SidebarLeft />
-				{/*<SidebarRight />*/}
+				<SidebarRight />
 			</div>
 		</>
 	);

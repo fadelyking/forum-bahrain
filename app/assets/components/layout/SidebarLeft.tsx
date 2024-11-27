3dfd3082366ce07adf431216040d67a5a1e2a514
fadelyking <fadely195@gmail.com>
2024-11-27 14:10:35 +0300

    remove search bar from sidebar

--- app/assets/components/layout/SidebarLeft.tsx
+++ app/assets/components/layout/SidebarLeft.tsx
@@ -9,28 +9,29 @@ export default function SidebarLeft() {
 	const categories = useCategoriesStore((state) => state.categoriesList);
 
 	return (
-		<Box className="col-start-6 col-end-7 max-w-full sticky">
-			<div className=" flex items-end justify-end">
-				<ul className="noto_kufi flex flex-col gap-3 font-bold justify-end items-end text-md p-6 bg-[#272727] text-[#bdbdbd] rounded-lg">
-					<div className="flex flex-col gap-3 items-center justify-center max-w-52">
-						<ForumButton>
-							<span className="text-white">
-								أنشئ منشوراً جديد
-							</span>
-						</ForumButton>
-						<SearchBar />
-					</div>
-					{categories.map((category) => (
-						<li
-							key={uuidv4()}
-							className="hover:text-white cursor-pointer"
-						>
-							<span>{category.icon}</span>
-							<span>{category.name}</span>
-						</li>
-					))}
-				</ul>
-			</div>
-		</Box>
+		<div className="col-start-6 col-span-1">
+			<Box>
+				<div className="flex items-end justify-end">
+					<ul className="noto_kufi flex flex-col gap-3 font-bold justify-end items-end text-md p-6 bg-[#272727] text-[#bdbdbd] rounded-lg">
+						<div className="flex flex-col gap-3 items-center justify-center">
+							<ForumButton>
+								<span className="text-white">
+									أنشئ منشوراً جديد
+								</span>
+							</ForumButton>
+						</div>
+						{categories.map((category) => (
+							<li
+								key={uuidv4()}
+								className="hover:text-white cursor-pointer text-end"
+							>
+								<span>{category.icon}</span>
+								<span>{category.name}</span>
+							</li>
+						))}
+					</ul>
+				</div>
+			</Box>
+		</div>
 	);
 }

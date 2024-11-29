225f325e8a430448149c7ec084621017aa96a126
fadelyking <fadely195@gmail.com>
2024-11-29 21:38:48 +0300

    redoing dialog from scratch

--- app/assets/components/common/LoginDialog.tsx
+++ app/assets/components/common/LoginDialog.tsx
@@ -2,19 +2,6 @@ import { Dialog } from "@radix-ui/themes";
 
 export default function LoginDialog() {
 	return (
-		<Dialog.Root>
-			<Dialog.Trigger>
-				<span>تسجيل الدخول</span>
-			</Dialog.Trigger>
 
-			<Dialog.Content size="3" className="noto_kufi text-center bg-black">
-				<Dialog.Title>سلام عليكم</Dialog.Title>
-				<Dialog.Description size="2" mb="4">
-					استخدم ايميلك او اي خدمة من الخدمات المتوفرة لتسجيل الدخول
-				</Dialog.Description>
-
-				<button>Continue with google</button>
-			</Dialog.Content>
-		</Dialog.Root>
 	);
 }

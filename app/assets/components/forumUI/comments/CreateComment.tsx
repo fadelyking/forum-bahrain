337f472d7304efa1ae2bbd025dbdc1f807cc6824
fadelyking <fadely195@gmail.com>
2024-11-30 17:32:03 +0300

    autofocus on writing comment

--- app/assets/components/forumUI/comments/CreateComment.tsx
+++ app/assets/components/forumUI/comments/CreateComment.tsx
@@ -5,6 +5,7 @@ export default function CreateComment() {
 				name="comments"
 				className="text-sm placeholder:text-sm text-end bg-[#363737] p-3 rounded-md w-full"
 				placeholder="...اترك تعليق"
+				autoFocus
 			/>
 			<button className="p-3 rounded-md bg-[#458b60] text-white hover:bg-[#5aa276] text-xs">
 				ارسل
